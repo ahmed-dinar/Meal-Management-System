2a20494ddfd6f41095ebd814079afe3ba7878649
ahmed-dinar <madinar.cse@gmail.com>
2015-03-03 02:11:30 +0600

    Update AboutMM.java

--- Meal Management System/src/Dialogs/AboutMM.java
+++ Meal Management System/src/Dialogs/AboutMM.java
@@ -93,7 +93,20 @@ public class AboutMM extends JFrame implements MouseListener {
 		/* licence panel */
 
 		
-		lincence.setText("");
+		lincence.setText(" Meal Management System is a non-commercial, open source utility application"
+			 + " to make maintain meal record,account record easier.I start this project"
+			 + " in my own interest. Copyright (C) 2015, Maid Ahmed Dinar.\n\n"
+			 + " This program is free software: you can redistribute it and/or modify"
+			 + " it under the terms of the GNU General Public License as published by"
+			 + " the Free Software Foundation, either version 3 of the License, or"
+			 + " (at your option) any later version.\n\n"
+			 + " This program is distributed in the hope that it will be useful,"
+			 + " but WITHOUT ANY WARRANTY; without even the implied warranty of"
+			 + " MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the"
+			 + " GNU General Public License for more details.\n\n"
+			 + " You should have received a copy of the GNU General Public License"
+			 + " along with this program. If not, see <http://www.gnu.org/licenses/>."
+		 );
 		
 		
 		/**************/
